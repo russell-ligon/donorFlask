4271fc9ca9ebf736e8889fd99e3b078d1412578a
russell-ligon <russell.ligon@gmail.com>
2020-06-23 09:54:19 -0400

    uncomment @app.route('/')

--- donorschoose/views.py
+++ donorschoose/views.py
@@ -77,7 +77,7 @@ with open('/home/russell/Documents/GitHub/donorFlask/donorschoose/static/thoroug
 
 
 
-# @app.route('/')
+@app.route('/')
 # @app.route('/index')
 # def index():
 #     return render_template("index.html",
